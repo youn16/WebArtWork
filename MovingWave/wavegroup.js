c1b9521442cb4138b4c332c3e3c81552fb3d4810
youn16 <seyounlee16@gmail.com>
2020-11-29 13:46:06 +0900

    update wavegroup.js

--- MovingWave/wavegroup.js
+++ MovingWave/wavegroup.js
@@ -4,8 +4,8 @@ export class WaveGroup {
     constructor() {
         this.totalWaves = 3;
         this.totalPoints = 6;
-
-        this.color = ['rgba(0,199,235,0.4)', 'rgba(0,146,199,0.4)', 'rgba(0,87,158,0.4)'];
+        // rgba(red,green,blue,alpha = 투명도)
+        this.color = ['rgba(100,200,0,0.4)', 'rgba(200,0,30,0.4)', 'rgba(0,200,30,0.4)'];
         this.waves = [];
 
         for (let i = 0; i < this.totalWaves; i++) {
